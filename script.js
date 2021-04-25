0b380db4f84f8d327d0855accc3e39193844ca0c
Danilo Souza dos Santos <danilosouza302@gmail.com>
2021-04-25 12:23:37 -0300

    Foram adicionados a Seçao de serviços, Área de contato e Responsividade

---
+++ script.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        })
+    })
+})
+
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = {
+        lat: -8.063169,
+        lng: -34.871139
+    };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
